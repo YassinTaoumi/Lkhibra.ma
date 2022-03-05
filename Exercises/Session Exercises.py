1b28c8ac629d328c99948a0fefb183115e66a7f3
yassin <toumiyassin84@gmail.com>
2022-03-05 17:43:06 +0100

    Python Training

--- Exercises/Session Exercises.py
+++ Exercises/Session Exercises.py
@@ -1,12 +1,63 @@
-# create Three variables where you'll store your first name, last name and Age and print 
-# them in the screen 
+"""
+Write a program that asks a user for his name and age
+and print them.
+"""
+
+
+
+
+"""
+check if he is his age is bigger than 20 or not  
+"""
+
+
+"""
+Write a program to accept two numbers from the user and calculate multiplication  
+"""
+
+
+
+"""
+Write a Python program which accepts the radius of a circle from the user 
+and compute the area.
+"""
 
 
 
 
 
+"""
+Print First 10 natural numbers using while loop
+"""
 
 
 
 
-#
+
+"""
+Write a Python program to sum all the items in a list.   
+"""
+
+
+
+
+"""
+Write a Python program to get the largest number from a list.   
+"""
+
+
+
+
+"""
+Write a Python program to get the smallest number from a list.  
+"""
+
+
+"""
+print the following pattern 
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+"""
