6d1beee001c51a244c43bc4acb27c041749343ba
Yassin Taoumi <76839849+YassinTaoumi@users.noreply.github.com>
2022-03-26 19:23:36 +0100

    Python Training

---
+++ Exercises/Session 3 Exersices/DuringSession.py
@@ -0,0 +1,48 @@
+"""
+Create a string made of the middle three characters
+input: str1 = "JhonDipPeta"
+output: Dip
+"""
+
+"""
+Create a new string made of the first, middle, and last characters of each input string
+input: s1 = "America"
+       s2 = "Japan"
+output: AJrpan
+"""
+
+"""
+Arrange string characters such that lowercase letters should come first
+input : str1 = IloVE PYtHon
+output: lotonIVEPYH
+
+"""
+
+"""
+Count all letters, digits, and special symbols from a given string
+input: str1 = "P@#yn26at^&i5ve"
+output : Chars = 8 
+         Digits = 3 
+         Symbol = 4
+"""
+
+"""
+Find all occurrences of a substring in a given string by ignoring the case
+"""
+
+"""
+Reverse a given string
+input: str1 = "I Love Python"
+output : onhtyP evoL I
+"""
+
+"""
+Split a string on hyphens
+input : str1 = Emma-is-a-data-scientist
+output : 
+Emma
+is
+a
+data
+scientist
+"""
---
+++ Exercises/Session 3 Exersices/Exercices.py
@@ -0,0 +1,53 @@
+"""
+Write a python program that counts the number of times the letter a appears in a
+string
+"""
+
+"""
+Create a string made of the first, middle and last character
+input : str1 = "James"
+output : Jms
+"""
+
+"""
+Append new string in the middle of a given string 
+input : s1 = "Ault"
+        s2 = "Kelly"    
+output : AuKellylt
+"""
+
+"""
+Create a mixed string from two strings 
+input : s1 = "Abc"
+        s2 = "Xyz"
+output: AzbycX
+"""
+
+"""
+Write a program to count occurrences of all characters within a string
+input : str1 = "Apple"
+output : {'A': 1, 'p': 2, 'l': 1, 'e': 1}
+"""
+
+"""
+Remove empty strings from a list of strings
+input : str_list = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+output:  ['Emma', 'Jon', 'Kelly', 'Eric']
+"""
+
+"""
+Removal all characters from a string except integers
+input : str1 = 'I am 25 years and 10 months old'
+output : 2510
+"""
+
+"""
+Write a program to count vowels and consonants in a string.
+input : python
+output: Vowels count is: 1
+        Consonant count is: 5   
+"""
+
+"""
+Write a program to remove duplicates in a string.
+"""
