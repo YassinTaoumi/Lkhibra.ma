75019e1f4817b2b53ee421cbe54c0b33bfd172c5
Yassin Taoumi <76839849+YassinTaoumi@users.noreply.github.com>
2022-04-02 19:28:51 +0000

    Python Training

---
+++ Exercises/Session 4 Exercises/DuringSession.py
@@ -0,0 +1,31 @@
+"""
+Write a program to create a function that takes two arguments, name and age
+and print their value.
+"""
+
+"""
+Write a function called city_country() that takes in the name
+of a city and its country. The function should return a string formatted like this:
+"Santiago, Chile"
+"""
+
+"""
+Define a function that accepts 2 values and return its sum, subtraction and multiplication.
+"""
+
+"""
+Define a function in python that accepts 3 values and returns the maximum of three numbers.
+"""
+
+"""
+ Define a function that accepts lowercase words and returns uppercase words.
+"""
+
+"""
+Define a function that accepts radius and returns the area of a circle.
+"""
+
+"""
+Make a list of magician’s names. Pass the list to a function
+called show_magicians() , which prints the name of each magician in the list.
+"""
---
+++ Exercises/Session 4 Exercises/Practice.py
@@ -0,0 +1,67 @@
+"""
+Write a program to create function calculation() such that it can accept two 
+variables and calculate addition and subtraction. Also, it must 
+return both addition and subtraction in a single return call.
+"""
+
+"""
+Write a program to create function func1() to accept a variable 
+length of arguments and print their value.
+"""
+
+"""
+Write a program to create a function show_employee() using the following conditions.
+It should accept the employee’s name and salary and display both.
+If the salary is missing in the function call then assign default value 9000 to salary
+"""
+
+"""
+Create an inner function to calculate the addition in the following way
+Create an outer function that will accept two parameters, a and b
+Create an inner function inside an outer function that will calculate the addition of a and b
+At last, an outer function will add 5 into addition and return it
+"""
+
+"""
+Create a function that Generate a Python list of all the even numbers between 4 to 30
+"""
+
+"""
+Define a function that accepts a number and returns whether the number is even or odd.
+"""
+
+"""
+Define a function which counts vowels and consonant in a word.
+"""
+
+"""
+Write a function called make_album() that builds a dictionary
+describing a music album. The function should take in an artist name and an
+album title, and it should return a dictionary containing these two pieces of
+information. Use the function to make three dictionaries representing different
+albums. Print each return value to show that the dictionaries are storing the
+album information correctly.
+Add an optional parameter to make_album() that allows you to store the
+number of tracks on an album. If the calling line includes a value for the number of tracks, 
+add that value to the album’s dictionary. Make at least one new
+function call that includes the number of tracks on an album.
+"""
+
+"""
+Write a function that stores information about a car in a diction-
+ary. The function should always receive a manufacturer and a model name. It
+should then accept an arbitrary number of keyword arguments. Call the func-
+tion with the required information and two other name-value pairs, such as a
+color or an optional feature. Your function should work for a call like this one:
+car = make_car('subaru', 'outback', color='blue', tow_package=True)
+Print the dictionary that’s returned to make sure all the information was
+stored correctly.
+"""
+
+"""
+Write a function that accepts a list of items a person wants
+on a sandwich. The function should have one parameter that collects as many
+items as the function call provides, and it should print a summary of the sand-
+wich that is being ordered. Call the function three times, using a different num-
+ber of arguments each time.
+"""
