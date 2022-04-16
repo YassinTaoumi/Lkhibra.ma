c4d5f9bfd8e00b1b2696b248c4009c7b1af3c700
Yassin Taoumi <76839849+YassinTaoumi@users.noreply.github.com>
2022-04-16 14:53:30 +0100

    Python Training

---
+++ Exercises/Session 5 Exercises/DuringSession.py
@@ -0,0 +1,32 @@
+"""
+Create a vehicule class that has : name, max_speed, color , capacity
+and create a method that prints the following message : 
+The seating capacity of a name is capacity passengers
+"""
+
+"""
+Create a Bus child class that inherits from the Vehicle class. 
+The default fare charge of any vehicle is seating capacity * 100. 
+If Vehicle is Bus instance, we need to add an extra 10% on full fare as a maintenance charge. 
+So total fare for bus instance will become the final amount = total fare + 10% of the total fare.
+Note: The bus seating capacity is 50. so the final fare amount should be 5500. 
+You need to override the fare() method of a Vehicle class in Bus class.
+"""
+
+"""
+Create an instance from the vehicule class and call the methods of this class for this object
+"""
+
+"""
+Make a class called User . Create two attributes called first_name
+and last_name , and then create several other attributes that are typically stored
+in a user profile. Make a method called describe_user() that prints a summary
+of the user’s information. Make another method called greet_user() that prints
+a personalized greeting to the user.
+Create several instances representing different users, and call both methods
+for each user.
+"""
+
+"""
+
+"""
---
+++ Exercises/Session 5 Exercises/Practice.py
@@ -0,0 +1,33 @@
+"""
+Make a class called Restaurant . The __init__() method for
+Restaurant should store two attributes: a restaurant_name and a cuisine_type .
+Make a method called describe_restaurant() that prints these two pieces of
+information, and a method called open_restaurant() that prints a message indi-
+cating that the restaurant is open.
+Make an instance called restaurant from your class. Print the two attri-
+butes individually, and then call both methods.
+--> Create three different instances from the class, and call describe_restaurant() for each
+instance
+"""
+
+"""
+Start with your program from the above exercise.
+Add an attribute called number_served with a default value of 0. Create an
+instance called restaurant from this class. Print the number of customers the
+restaurant has served, and then change this value and print it again.
+Add a method called set_number_served() that lets you set the number
+of customers that have been served. Call this method with a new number and
+print the value again.
+Add a method called increment_number_served() that lets you increment
+the number of customers who’ve been served. Call this method with any num-
+ber you like that could represent how many customers were served in, say, a
+day of business.
+"""
+
+"""
+Create a class called person, each person has the following attributes: 
+name, surname, birthdate, address, telephone, email
+every person’s age is calculated for the first time when a new person instance is
+created, and recalculated (when it is requested) if the day has changed since the last time
+ that it was calculated. 
+"""
